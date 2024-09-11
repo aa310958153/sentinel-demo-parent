5355db19747bf4138e032c029aebe553478cbe79
liqiang <liqiang2@yxt.wecom.work>
2024-09-11 18:48:06 +0800

    feat:记录qps限制是线程维度

--- sentinel-helloword-demo/src/test/java/com/example/sentinelhelloworddemo/service/AssistCommodityAdapterTest.java
+++ sentinel-helloword-demo/src/test/java/com/example/sentinelhelloworddemo/service/AssistCommodityAdapterTest.java
@@ -12,22 +12,21 @@ import com.alibaba.csp.sentinel.slots.block.degrade.DegradeRuleManager;
 import com.alibaba.csp.sentinel.slots.block.degrade.circuitbreaker.CircuitBreaker.State;
 import com.alibaba.csp.sentinel.slots.block.degrade.circuitbreaker.CircuitBreakerStrategy;
 import com.alibaba.csp.sentinel.slots.block.degrade.circuitbreaker.EventObserverRegistry;
+import com.alibaba.csp.sentinel.slots.block.flow.ClusterFlowConfig;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 import com.alibaba.csp.sentinel.slots.block.flow.param.ParamFlowItem;
 import com.alibaba.csp.sentinel.slots.block.flow.param.ParamFlowRule;
 import com.alibaba.csp.sentinel.slots.block.flow.param.ParamFlowRuleManager;
-import com.alibaba.csp.sentinel.slots.system.SystemRule;
-import com.alibaba.csp.sentinel.slots.system.SystemRuleManager;
 import com.alibaba.csp.sentinel.util.TimeUtil;
+import com.alibaba.fastjson.JSON;
 import com.example.sentinelhelloworddemo.SentinelHellowordDemoApplicationTests;
 import com.example.sentinelhelloworddemo.constans.SentinelConstants;
-
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import javax.annotation.Resource;
-
-import com.example.sentinelhelloworddemo.util.FileUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.reflect.Whitebox;
@@ -46,94 +45,55 @@ public class AssistCommodityAdapterTest {
     private AssistCommodityAdapter adapter;
 
     /**
-     * 配合控制台使用:
-     * 文档参考：https://sentinelguard.io/zh-cn/docs/dashboard.html
+     * 初始化流控规则 [ { "app": "assist-core-toolkit-local", "clusterConfig": { "fallbackToLocalWhenFail": true,
+     * "sampleCount": 10, "strategy": 0, "thresholdType": 0, "windowIntervalMs": 1000 }, "clusterMode": false,
+     * "controlBehavior": 0, "count": 1.0, "gmtCreate": 1725876637357, "gmtModified": 1725876637357, "grade": 1, "id":
+     * 27, "ip": "10.4.1.125", "limitApp": "default", "port": 8719, "resource":
+     * "com.yxt.assist.toolkit.dao.coldchain.monitoring.dao.impl.ColdChainAlarmOrderDaoImpl.pageAlarmOrder", "strategy":
+     * 0 } ]
      */
-    @Test
-    public void flowRule_TEST() {
+    private static void initFlowRules() {
+        FlowRule flowRule = JSON.parseObject(" {\n"
+            + "        \"app\": \"assist-core-toolkit-local\",\n"
+            + "        \"clusterConfig\": {\n"
+            + "            \"fallbackToLocalWhenFail\": true,\n"
+            + "            \"sampleCount\": 10,\n"
+            + "            \"strategy\": 0,\n"
+            + "            \"thresholdType\": 0,\n"
+            + "            \"windowIntervalMs\": 1000\n"
+            + "        },\n"
+            + "        \"clusterMode\": false,\n"
+            + "        \"controlBehavior\": 0,\n"
+            + "        \"count\": 1.0,\n"
+            + "        \"gmtCreate\": 1725876637357,\n"
+            + "        \"gmtModified\": 1725876637357,\n"
+            + "        \"grade\": 1,\n"
+            + "        \"id\": 27,\n"
+            + "        \"ip\": \"10.4.1.125\",\n"
+            + "        \"limitApp\": \"default\",\n"
+            + "        \"port\": 8719,\n"
+            + "        \"resource\": \"com.yxt.assist.toolkit.dao.coldchain.monitoring.dao.impl.ColdChainAlarmOrderDaoImpl.pageAlarmOrder\",\n"
+            + "        \"strategy\": 0\n"
+            + "    }", FlowRule.class);
+        List<FlowRule> rules = new ArrayList<FlowRule>();
+        FlowRule rule1 = new FlowRule();
+        rule1.setResource("HelloWorld");
 
-        // 配置规则.
-        initFlowRules();
-        int i = 0;
-        while (true) {
-
-            // 1.5.0 版本开始可以直接利用 try-with-resources 特性 自动回收调用  entry.exit() 配合注解使用 @SentinelResource("HelloWorld")
-            try ( Entry entry = SphU.entry("HelloWorld")) {
-                // 被保护的逻辑
-                System.out.println("hello world" + (++i));
-            } catch (BlockException ex) {
-                i = 0;
-                // 处理被流控的逻辑
-                System.out.println("blocked!");
-
-            }
-        }
-    }
-
-    public void testParamFlowRules() throws IOException {
-        Entry entry = null;
-        try {
-            entry = SphU.entry("paramFlowRules", EntryType.IN, 1, "paramA", "paramB");
-            // Your logic here.
-        } catch (BlockException ex) {
-            // 处理被流控的逻辑
-            System.out.println("blocked!");
-        } finally {
-            if (entry != null) {
-                entry.exit(1, "paramA", "paramB");
-            }
-        }
-    }
-
-        /**
-         * 模拟熔断降级
-         * 经过测试熔断降级只会对请求做统计，而不会主动结束线程，比如设置5秒钟为慢请求，执行了10秒，需要等待10秒执行完，进行统计
-         * 如果瞬时流量瞬间增加，则会阻塞等待,调用第三方需要设置合理的超时时间
-         * @throws InterruptedException
-         */
-        @Test
-        public void degradeRule_TEST () throws InterruptedException {
-            initDegradeRule();
-            AssistCommodityQueryApi mockAssistCommodityQueryApi = new MockAssistCommodityQueryApiImpl();
-            Whitebox.setInternalState(adapter, "assistCommodityQueryApi",
-                mockAssistCommodityQueryApi);
-            //模拟5个线程并发调用
-            int concurrency = 5;
-            List<Thread> tasks = new ArrayList<>();
-            for (int i = 0; i < concurrency; i++) {
-                Thread entryThread = new Thread(() -> {
-                    while (true) {
-                        try {
-                            adapter.queryCommoditySale(null);
-                        } catch (Exception e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                });
-                entryThread.setName("sentinel-simulate-traffic-task-" + i);
-                entryThread.start();
-                tasks.add(entryThread);
-            }
-            for (Thread task : tasks) {
-                task.join();
-            }
-        }
-
-
-
-
-    
-    /**
-     * 初始化流控规则
-     */
-    private static void initFlowRules(){
-        List<FlowRule> rules = new ArrayList<>();
-        FlowRule rule = new FlowRule();
-        rule.setResource("HelloWorld");
-        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
-        // Set limit QPS to 20.
-        rule.setCount(20);
-        rules.add(rule);
+        ClusterFlowConfig config = new ClusterFlowConfig();
+        config.setFallbackToLocalWhenFail(true);
+        config.setSampleCount(10);
+        config.setThresholdType(0);
+        config.setWindowIntervalMs(1000);
+        rule1.setClusterConfig(config);
+        // set limit qps to 20
+        rule1.setCount(1);
+        rule1.setStrategy(0);
+        rule1.setClusterMode(false);
+        rule1.setControlBehavior(0);
+        rule1.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        rule1.setLimitApp("default");
+        rules.add(rule1);
+        rules.add(flowRule);
         FlowRuleManager.loadRules(rules);
     }
 
@@ -174,8 +134,95 @@ public class AssistCommodityAdapterTest {
     }
 
     /**
-     * com.alibaba.csp.sentinel.slots.block.flow.param.ParamFlowSlot#checkFlow
-     * 默认是string.valueof
+     * 配合控制台使用: 文档参考：https://sentinelguard.io/zh-cn/docs/dashboard.html
+     */
+    @Test
+    public void flowRule_TEST() throws InterruptedException {
+
+        // 配置规则.
+        initFlowRules();
+
+        for (int j = 0; j < 2; j++) {
+            new Thread(new Runnable() {
+                int i = 0;
+
+                @Override
+                public void run() {
+                    for (int k = 0; k < 30; k++) {
+                        // 1.5.0 版本开始可以直接利用 try-with-resources 特性 自动回收调用  entry.exit() 配合注解使用 @SentinelResource("HelloWorld")
+                        try (Entry entry = SphU.entry(
+                            "com.yxt.assist.toolkit.dao.coldchain.monitoring.dao.impl.ColdChainAlarmOrderDaoImpl.pageAlarmOrder",
+                            EntryType.IN)) {
+                            // 被保护的逻辑
+                            System.out.println(Thread.currentThread().getId() + "hello world" + (++i));
+
+                        } catch (BlockException ex) {
+                            i++;
+                            // 处理被流控的逻辑
+                            System.out.println("blocked!");
+
+                        }
+                    }
+
+                }
+            }).start();
+
+        }
+        Thread.sleep(500000000);
+    }
+
+
+    public void testParamFlowRules() throws IOException {
+        Entry entry = null;
+        try {
+            entry = SphU.entry("paramFlowRules", EntryType.IN, 1, "paramA", "paramB");
+            // Your logic here.
+        } catch (BlockException ex) {
+            // 处理被流控的逻辑
+            System.out.println("blocked!");
+        } finally {
+            if (entry != null) {
+                entry.exit(1, "paramA", "paramB");
+            }
+        }
+    }
+
+    /**
+     * 模拟熔断降级 经过测试熔断降级只会对请求做统计，而不会主动结束线程，比如设置5秒钟为慢请求，执行了10秒，需要等待10秒执行完，进行统计 如果瞬时流量瞬间增加，则会阻塞等待,调用第三方需要设置合理的超时时间
+     *
+     * @throws InterruptedException
+     */
+    @Test
+    public void degradeRule_TEST() throws InterruptedException {
+        initDegradeRule();
+        AssistCommodityQueryApi mockAssistCommodityQueryApi = new MockAssistCommodityQueryApiImpl();
+        Whitebox.setInternalState(adapter, "assistCommodityQueryApi",
+            mockAssistCommodityQueryApi);
+        //模拟5个线程并发调用
+        int concurrency = 5;
+        List<Thread> tasks = new ArrayList<>();
+        for (int i = 0; i < concurrency; i++) {
+            Thread entryThread = new Thread(() -> {
+                while (true) {
+                    try {
+                        adapter.queryCommoditySale(null);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+            entryThread.setName("sentinel-simulate-traffic-task-" + i);
+            entryThread.start();
+            tasks.add(entryThread);
+        }
+        for (Thread task : tasks) {
+            task.join();
+        }
+    }
+
+    /**
+     * com.alibaba.csp.sentinel.slots.block.flow.param.ParamFlowSlot#checkFlow 默认是string.valueof
+     *
      * @throws IOException
      */
     public void initParamFlowRules() throws IOException {
@@ -191,12 +238,12 @@ public class AssistCommodityAdapterTest {
 
         ParamFlowRuleManager.loadRules(Collections.singletonList(rule));
     }
+
     /**
-     * 资源应用仅允许appA,appB访问
-     * 调用方信息通过 ContextUtil.enter(resourceName, origin) 方法中的 origin 参数传入。
+     * 资源应用仅允许appA,appB访问 调用方信息通过 ContextUtil.enter(resourceName, origin) 方法中的 origin 参数传入。
      * ContextUtil.enter(resourceName, origin)
      */
-    public void intAuthorityRule(){
+    public void intAuthorityRule() {
         AuthorityRule rule = new AuthorityRule();
         rule.setResource("test");
         rule.setStrategy(RuleConstant.AUTHORITY_WHITE);
